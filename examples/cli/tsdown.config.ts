5f63cf8f78bea0716a3d1566262c546569d3f696
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2025-10-26 14:48:56 +1100

    Bundle cli

--- examples/cli/tsdown.config.ts
+++ examples/cli/tsdown.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "tsdown";
 export default defineConfig({
   entry: ["./src/index.ts"],
   format: ["esm"],
-  unbundle: true,
+  unbundle: false,
   dts: true,
   sourcemap: true,
   clean: true,
