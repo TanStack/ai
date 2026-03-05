e2862f4c81b18d28df776444c37cd94b23111245
Alem Tuzlak <t.zlak@hotmail.com>
2026-03-05 13:44:59 +0100

    chore: fix solid bundling

--- packages/typescript/ai-devtools/src/core.ts
+++ packages/typescript/ai-devtools/src/core.ts
@@ -2,8 +2,9 @@ import { constructCoreClass } from '@tanstack/devtools-utils/solid/class'
 
 export interface AiDevtoolsInit {}
 
+const importPath = "./components/Shell"
 const [AiDevtoolsCore, AiDevtoolsCoreNoOp] = constructCoreClass(
-  () => import('./components/Shell'),
+  () => import(importPath),
 )
 
 export { AiDevtoolsCore, AiDevtoolsCoreNoOp }
